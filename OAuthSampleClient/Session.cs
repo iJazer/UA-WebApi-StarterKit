92a4bd6069079f5a00ccb7538e04a942a683eea0
Randy Armstrong <randy@sparhawksoftware.com>
2023-05-04 04:25:31 -0700

    Add OAuth2SampleClient.

---
+++ OAuthSampleClient/Session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuthSampleClient
+{
+    public class Session
+    {
+        public string Id { get; set; }
+
+        public string AccessToken { get; set; }
+
+        public DateTime? ExpiresBy { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ OAuthSampleClient/Settings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OAuthSampleClient
+{
+    public class Settings
+    {
+        public string OAuthServerUrl { get; set; }
+
+        public string OAuthClientId { get; set; }
+
+        public string OAuthClientSecret { get; set; }
+
+        public static Settings LoadSettings()
+        {
+            // Load settings
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddUserSecrets<Settings>()
+                .Build();
+
+            return config.GetRequiredSection("Settings").Get<Settings>();
+        }
+    }
+}
