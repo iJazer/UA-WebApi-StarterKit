d0841e91ae4c2d5426223948b92ebe7b1bcc1fdd
Nils Baur <nils.baur@siemens.com>
2025-04-02 16:07:08 +0200

    adjust serverURL for client

--- UaWebApiGateway/UaRestGateway.Server/Service/UAClient.cs
+++ UaWebApiGateway/UaRestGateway.Server/Service/UAClient.cs
@@ -43,7 +43,8 @@ namespace UaRestGateway.Server.Service
             var configSectionName = "Quickstarts.ReferenceClient";
 
             //Change Server URL
-            Uri serverUrl = new Uri("opc.tcp://WhiteCat:48030/");
+            //Uri serverUrl = new Uri("opc.tcp://WhiteCat:48030/");
+            Uri serverUrl = new Uri("opc.tcp://192.168.119.131:4840");
 
             CertificatePasswordProvider PasswordProvider = new CertificatePasswordProvider(password);
             ApplicationInstance application = new ApplicationInstance
